b69df2f8e88fbeb49cfb76a9d1387ea9c2f9b96f
Abdullah <abdullahrehman1019@gmail.com>
2026-05-21 12:45:52 +0500

    initial commit

---
+++ lib/models/task.dart
@@ -0,0 +1,37 @@
+// To parse this JSON data, do
+//
+//     final taskModel = taskModelFromJson(jsonString);
+
+import 'dart:convert';
+
+class TaskModel {
+  final String? docId;
+  final String? title;
+  final String? description;
+  final bool? isCompleted;
+  final int? createdAt;
+
+  TaskModel({
+    this.docId,
+    this.title,
+    this.description,
+    this.isCompleted,
+    this.createdAt,
+  });
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) => TaskModel(
+    docId: json["docID"],
+    title: json["title"],
+    description: json["description"],
+    isCompleted: json["isCompleted"],
+    createdAt: json["createdAt"],
+  );
+
+  Map<String, dynamic> toJson(String taskID) => {
+    "docID": taskID,
+    "title": title,
+    "description": description,
+    "isCompleted": isCompleted,
+    "createdAt": createdAt,
+  };
+}
