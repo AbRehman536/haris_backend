ab2b2a14c42b08263e5cdd066d6d2de79ba980c2
Abdullah <abdullahrehman1019@gmail.com>
2026-05-25 12:50:47 +0500

    services done

---
+++ lib/services/task.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/task.dart';
+
+class TaskService{
+  String taskCollection = "TaskCollection";
+
+  ///Create Task
+  Future createTask(TaskModel model)async{
+   return await FirebaseFirestore.instance
+       .collection(taskCollection)
+       .add(model.toJson());
+  }
+  ///Update Task
+  Future updateTask(TaskModel model)async{
+    return await FirebaseFirestore.instance
+        .collection(taskCollection)
+        .doc(model.docId)
+        .update({"title" : model.title, "description" : model.description});
+  }
+  ///Delete Task
+  Future deleteTask(String taskID)async{
+    return await FirebaseFirestore.instance
+        .collection(taskCollection)
+        .doc(taskID)
+        .delete();
+  }
+  ///Mark As Completed
+  Future markAsCompletedTask(String taskID, bool isCompleted)async{
+    return await FirebaseFirestore.instance
+        .collection(taskCollection)
+        .doc(taskID)
+        .update({"isCompleted" : isCompleted,});
+  }
+  ///Get All Task
+  Stream<List<TaskModel>> getAllTask(){
+    return FirebaseFirestore.instance
+        .collection(taskCollection)
+        .snapshots()
+        .map((taskList) => taskList.docs
+        .map((taskJson) => TaskModel.fromJson(taskJson.data())).toList());
+  }
+  ///Get Completed Task
+  Stream<List<TaskModel>> getCompletedTask(){
+    return FirebaseFirestore.instance  //Connection with firebase firestore
+        .collection(taskCollection) //Table-Collection Name
+        .where("isCompleted", isEqualTo: true) // Query Condition
+        .snapshots() // Real Time Updated Data Show
+        .map((taskList) => taskList.docs // Copy Document Data and store in taskList Variable
+        .map((taskJson) => TaskModel.fromJson(taskJson.data())).toList()); // Convert Json Data to TaskModel
+  }
+  ///Get InCompleted Task
+  Stream<List<TaskModel>> getInCompletedTask(){
+    return FirebaseFirestore.instance
+        .collection(taskCollection)
+        .where("isCompleted", isEqualTo: false)
+        .snapshots()
+        .map((taskList) => taskList.docs
+        .map((taskJson) => TaskModel.fromJson(taskJson.data())).toList());
+  }
+}
